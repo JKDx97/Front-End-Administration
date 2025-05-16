2222ac16f02e51bd2ee5f7bd5d12433f8a0af1af
JKDx97 <hoterosebastian2004@gmail.com>
2025-05-16 15:55:51 -0500

    Actualizacion redirect

--- src/app/detalle.service.ts
+++ src/app/detalle.service.ts
@@ -1,18 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {environment} from './environments/environment'
+import { environment } from './environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DetalleService {
+  private apiUrlTop = `${environment.api}/detalle/top`;
+  private apiUrlDetalle = `${environment.api}/detalle`;
 
-  private apiUrlDetalle = `${environment.api}/detalle`
+  constructor(private http: HttpClient) {}
 
-  constructor(private http : HttpClient) { }
+  getTop() {
+    return this.http.get<{ producto: string; total_vendidos: number }[]>(
+      this.apiUrlTop
+    );
+  }
 
-  getDetalle(venta_id : number):Observable<any[]>{
-    return this.http.get<any[]>(`${this.apiUrlDetalle}/${venta_id}`)
+  getDetalle(venta_id: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrlDetalle}/${venta_id}`);
   }
 }
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DetalleService } from '../detalle.service';
+import { NgChartsModule } from 'ng2-charts';
+import { ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, NgChartsModule],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrl: './index.component.css',
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  productosLabels: string[] = [];
+  productosData: number[] = [];
 
+  chartType: ChartType = 'bar';
+
+  chartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    },
+  };
+
+  constructor(private detalleService: DetalleService) {}
+
+  ngOnInit(): void {
+    this.getTop(); // Cargar datos automáticamente al iniciar
+  }
+
+  get chartData() {
+    return {
+      labels: this.productosLabels,
+      datasets: [
+        {
+          label: 'Productos más vendidos',
+          data: this.productosData,
+          backgroundColor: [
+            '#42A5F5', '#66BB6A', '#FFA726', '#EF5350', '#AB47BC',
+            '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'
+          ],
+        },
+      ],
+    };
+  }
+
+  getTop() {
+    this.detalleService.getTop().subscribe((data) => {
+      this.productosLabels = data.map((item) => item.producto);
+      this.productosData = data.map((item) => item.total_vendidos);
+    });
+  }
 }
